eec0632f7ba8f8ccd95da91c9c2b508f9c5ef7e8
DAVID LIMON <tu_correo@ejemplo.com>
2026-06-14 17:25:06 -0600

    Actualizacion automatica

--- subir_quiniela.bat
+++ subir_quiniela.bat
@@ -1,7 +1,7 @@
 @echo off
-git add quiniela_actualizada_2026.xlsx
-git commit -m "Actualizacion automatica de quiniela"
-git push origin main
+git add .
+git commit -m "Actualizacion automatica"
+git push origin master
 echo.
 echo ¡Listo! La quiniela ha sido subida a GitHub.
 pause
